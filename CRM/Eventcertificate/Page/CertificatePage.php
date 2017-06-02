4d26edc5f1d66416aed5e003ef5815288b9dea22
Alice Frumin <alice@aghstrategies.com>
2017-06-02 13:37:29 -0400

    when you visit page shows pdf and downloads pdf

--- CRM/Eventcertificate/Page/CertificatePage.php
+++ CRM/Eventcertificate/Page/CertificatePage.php
@@ -1,7 +1,40 @@
 <?php
 
 class CRM_Eventcertificate_Page_CertificatePage extends CRM_Core_Page {
-  public function getHTMLwithTokens() {
+
+  public function textToDisplay() {
+    // Default text
+    $textToDisplay = array(
+      'pdf' => 0,
+      'text' => "not enough information to process your request",
+    );
+    // Check for contact id and event id
+    if (!empty($_GET["cid"]) && !empty($_GET["eid"])) {
+      $contactId = $_GET["cid"];
+      $eventId = $_GET["eid"];
+      try {
+        $participant = civicrm_api3('Participant', 'get', array(
+          'sequential' => 1,
+          'contact_id' => $_GET["cid"],
+          'event_id' => $_GET["eid"],
+          'status_id' => "Registered",
+          'role_id' => "Attendee",
+        ));
+      }
+      catch (CiviCRM_API3_Exception $e) {
+        $error = $e->getMessage();
+        CRM_Core_Error::debug_log_message(t('API Error: %1', array(1 => $error, 'domain' => 'com.aghstrategies.eventcertificate')));
+      }
+      // check that the contact is a registered attendee for the event
+      if (!empty($participant['values'][0])) {
+        $textToDisplay['pdf']  = 1;
+        $textToDisplay['text'] = self::getHTMLwithTokens($contactId);
+      }
+    }
+    return $textToDisplay;
+  }
+
+  public function getHTMLwithTokens($contactId) {
     try {
       $result = civicrm_api3('MessageTemplate', 'getsingle', array(
         'sequential' => 1,
@@ -13,9 +46,6 @@ class CRM_Eventcertificate_Page_CertificatePage extends CRM_Core_Page {
       CRM_Core_Error::debug_log_message(t('API Error: %1', array(1 => $error, 'domain' => 'com.aghstrategies.eventcertificate')));
     }
     $html_message = $result['msg_html'];
-    $contactId = 203;
-    // CRM_Contact_Form_Task_PDFLetterComon::formatMessage($html_message);
-    // print_r($html_message); die();
 
     $messageToken = CRM_Utils_Token::getTokens($result['msg_html']);
     $returnProperties = array();
@@ -26,8 +56,9 @@ class CRM_Eventcertificate_Page_CertificatePage extends CRM_Core_Page {
     }
     $categories = array();
     $formValues = $result['values'][0];
-    $params = array('contact_id' => 203);
-    list($contact) = CRM_Utils_Token::getTokenDetails($params,
+    $params = array('contact_id' => $contactId);
+    list($contact) = CRM_Utils_Token::getTokenDetails(
+      $params,
       $returnProperties,
       TRUE,
       TRUE,
@@ -35,19 +66,19 @@ class CRM_Eventcertificate_Page_CertificatePage extends CRM_Core_Page {
       $messageToken,
       'CRM_Contact_Form_Task_PDFLetterCommon'
     );
+    // print_r($contact); die();
     $tokenHtml = CRM_Utils_Token::replaceContactTokens($html_message, $contact[$contactId], TRUE, $messageToken);
     return $tokenHtml;
   }
+
   public function run() {
-    $tokenHtml = self::getHTMLwithTokens();
-    // CRM_Utils_PDF_Utils::html2pdf($html, "CiviLetter.pdf", FALSE, $formValues);
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('Certificate Page'));
-
-    // Example: Assign a variable for use in a template
+    $textToDisplay = self::textToDisplay();
     $this->assign('currentTime', date('Y-m-d H:i:s'));
-    $this->assign('messageHtml', $tokenHtml);
-
+    $this->assign('messageHtml', $textToDisplay['text']);
+    if ($textToDisplay['pdf'] == 1) {
+      CRM_Utils_PDF_Utils::html2pdf($textToDisplay['text'], "CiviEventCertificate.pdf", FALSE, $formValues);
+    }
     parent::run();
   }
 
--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 Page created by extension:
 
-http://wpmaster/wp-admin/admin.php?page=CiviCRM&q=civicrm%2Feventcertificate
+http://wpmaster/wp-admin/admin.php?page=CiviCRM&q=civicrm%2Feventcertificate&cid=203&eid=2
 
 Things to do:
 
-1. Pass event id and and contact id thru url
 2. create a button on the page to download the template as a pdf (https://github.com/civicrm/civicrm-core/blob/c1cc5247b974a534b24fb4e9315d82e276fecea6/CRM/Contact/Form/Task/PDFLetterCommon.php#L402)
 3. on install create a system workflow message template to be used by this extension
-4. write logic for when you see certificate and message for if you dont
+4. write logic for when you are logged in
