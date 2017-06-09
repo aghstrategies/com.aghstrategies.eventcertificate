ab644188343d1188d03755bb2554af647d22d306
Alice Frumin <alice@aghstrategies.com>
2017-06-09 13:04:01 -0400

    certificate download

--- CRM/Eventcertificate/Page/CertificatePage.php
+++ CRM/Eventcertificate/Page/CertificatePage.php
@@ -154,9 +154,9 @@ class CRM_Eventcertificate_Page_CertificatePage extends CRM_Core_Page {
     // $this->assign('currentTime', date('Y-m-d H:i:s'));
     $this->assign('messageHtml', $textToDisplay['text']);
     // will download the pdf when you go to this url
-    // if ($textToDisplay['pdf'] == 1) {
-    //   CRM_Utils_PDF_Utils::html2pdf($textToDisplay['text'], "CiviEventCertificate.pdf", FALSE, $formValues);
-    // }
+    if ($textToDisplay['pdf'] == 1) {
+      CRM_Utils_PDF_Utils::html2pdf($textToDisplay['text'], "CiviEventCertificate.pdf", FALSE, $formValues);
+    }
     parent::run();
   }
 
