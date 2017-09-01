63eed59cebc14cc38b934f5e8fa1acbac3f6e16d
Alice Frumin <alice@aghstrategies.com>
2017-09-01 12:36:28 -0400

    AGH #10658 adding this to deal with permission error in 4.7.23 hopefully can trash when bug gets fixed

--- eventcertificate.php
+++ eventcertificate.php
@@ -2,6 +2,15 @@
 
 require_once 'eventcertificate.civix.php';
 
+/*adding this to deal with permission error in 4.7.23 hopefully can trash when bug gets fixed*/
+function eventcertificate_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions)
+{
+  // skipping permission to get events thru api because of weird bug in 4.7.232
+  if ($entity == 'event' && $action == 'get') {
+      $params['check_permissions'] = false;
+  }
+}
+
 function eventcertificate_checkIfExsisting($entity, $params, $delete = FALSE) {
   $params['sequential'] = 1;
   $recordFound = NULL;
